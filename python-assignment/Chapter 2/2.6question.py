5ad964048e046c9cc88bfaac6e450b00115d709a
sideeqa <sideeqahabdulkareem@gmail.com>
2025-12-05 02:13:28 +0100

    new

---
+++ python-assignment/Chapter 2/2.6question.py
@@ -0,0 +1,13 @@
+2.6 collect a number from user, store in variables"number"
+    if number divided by 2 is remainder of 0, print "Even"
+
+else
+       print ("odd")
+
+   number= int(input("Enter first number:"))
+   if (number%2==0):
+       print ("even")
+    
+   else
+       print ("odd")
+
---
+++ python-assignment/Chapter 2/2.8question.py
@@ -0,0 +1,8 @@
+print ("number/tsquare/ttable")
+print ("0/t+str(0**2)+"/t"str(0**3))
+print ("0/t+str(1**2)+"/t"str(1**3)) 
+print ("0/t+str(2**2)+"/t"str(2**3))  
+print ("0/t+str(3**2)+"/t"str(3**3))
+print ("0/t+str(4**2)+"/t"str(4**3)) 
+print ("0/t+str(5**2)+"/t"str(5**3))  
+ 
